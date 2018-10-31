5ec2f7ff0750de13bd4716ae39264aba52c73b3f
tp6vup54 <genius091612@gmail.com>
2018-10-31 19:06:12 +0800

    Update.

--- 1. Shopping cart schema/README.md
+++ 1. Shopping cart schema/README.md
@@ -2,6 +2,6 @@
 ### ER Disgram
 <img src="https://i.imgur.com/EHOq4rM.png"/>
 
-For the index in `tb_item`, I think `ItemName` and  `Price` are often be queried in the system, including orders history page and item detail page.
+For the index in `tb_item`, I think `ItemName` and  `Price` are often be queried in the system, including order history page and item detail page.
 
 
---
+++ 1. Shopping cart schema/scenarios.sql
@@ -0,0 +1,32 @@
+-- Query order history
+-- Here I suppose that current user can be queried from CurrentUser.UserGuid.
+SELECT a.Receiver, a.Address, a.OrderedDate, SUM(b.Price * b.Count)
+FROM tb_Order AS a, tb_OrderedItem AS b
+WHERE a.UserGuid = CurrentUser.UserGuid
+AND a.OrderGuid = b.OrderGuid
+ORDER BY a.OrderedDate
+
+
+-- Add shop item into shopping cart
+-- Here I suppose that current selected item can be queried from CurrentItem.ItemGuid.
+INSERT INTO tb_CartItemRelation(CiGuid, CartGuid, ItemGuid, Count)
+    SELECT NEWID(), CartGuid, CurrentItem.ItemGuid, 1
+    FROM tb_Cart
+    WHERE UserGuid = CurrentUser.UserGuid
+
+
+-- 
+DECLARE @guid CHAR(36)
+
+SET @guid = NEWID()
+
+INSERT INTO tb_Order(OrderGuid, UserGuid, Receiver, Address, OrderedDate)
+    SELECT @guid, CurrentUser.UserGuid, Receiver.Name, Address, GETDATE()
+    FROM tb_Address
+    WHERE AddressGuid = SelectedAddress.AddressGuid
+
+INSERT INTO tb_OrderedItem(OrderItemGuid, ItemGuid, OrderGuid, ItemName, Price, Count)
+    SELECT NEWID(), b.ItemGuid, @guid, c.ItemName, c.Price, a.Count
+    FROM tb_CartItemRelation AS a, tb_Cart AS b
+    INNER JOIN tb_Item AS c ON c.ItemGuid = a.ItemGuid
+    WHERE b.UserGuid = CurrentUser.UserGuid AND a.CartGuid = b.CartGuid
